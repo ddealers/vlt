df7172e5f7fa5088adc75d438ab28f07c4089042
Wenceslao Negrete <vampediem@gmail.com>
2014-08-21 16:46:45 -0500

    about page

--- about.php
+++ about.php
@@ -15,10 +15,10 @@
     <body class="about">
         <div class="navbar-wrapper">
       		<div class="container">
-        		     <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+        		<div class="navbar navbar-inverse navbar-static-top" role="navigation">
           			<div class="container">
-            			     <div class="navbar-header">
-              				     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            			<div class="navbar-header">
+              				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 			                	<span class="sr-only">Toggle navigation</span>
 			                	<span class="icon-bar"></span>
 			                	<span class="icon-bar"></span>
@@ -28,28 +28,29 @@
             			</div>
             			<div class="navbar-collapse collapse">
               				<ul class="nav navbar-nav">
-				                <li class="active"><a href="#">ABOUT US</a></li>
-				                <li><a href="#about">CLIENTS</a></li>
-				                <li><a href="#contact">WORK</a></li>
-				                <li class="dropdown">
+				            	<li class="active"><a href="#">ABOUT US</a></li>
+				            	<li><a href="#about">CLIENTS</a></li>
+				            	<li><a href="#contact">WORK</a></li>
+				            	<li class="dropdown">
                   					<a href="#" class="dropdown-toggle" data-toggle="dropdown">CONTACT<span class="caret"></span></a>
                   					<ul class="dropdown-menu" role="menu">
-					                    <li><a href="#" class="facebook">Facebook</a></li>
-					                    <li><a href="#" class="twitter">Twitter</a></li>
-					                    <li><a href="#" class="linkedin">Linkedin</a></li>
-					                 	<li><a href="#" class="news">News</a></li>
+                  						<li><a href="#" class="facebook">Facebook</a></li>
+                  						<li><a href="#" class="twitter">Twitter</a></li>
+                  						<li><a href="#" class="linkedin">Linkedin</a></li>
+                  						<li><a href="#" class="news">News</a></li>
                   					</ul>
                 				</li>
               				</ul>
-                              <ul class="icon-r-social">
-                                   <li class="new">News <a href="#" class="news-n"></a> </li>
-                                   <li> <a href="#" class="facebook-f"></a> </li>
-                                   <li> <a href="#" class="twitter-t"></a> </li>
-                                   <li> <a href="#" class="linkedin-l"></a> </li>
-                              </ul>
+              				<ul class="icon-r-social">
+              					<li class="new">News <a href="#" class="news-n"></a></li>
+              					<li><a href="#" class="facebook-f"></a></li>
+              					<li><a href="#" class="twitter-t"></a></li>
+              					<li><a href="#" class="linkedin-l"></a></li>
+              				</ul>
             			</div>
-          		</div>
+          			</div>
         		</div>
+        	</div>
       	</div>
         <div class="main container-fluid">
             <div class="row">
