a0064a0478ab09c389da2c245eb560436f537405
aresavazquez <ares _vb>
2014-08-22 02:09:37 -0500

    final vlt

--- includes/navbar.php
+++ includes/navbar.php
@@ -18,20 +18,20 @@
                       <li><a class="page-scroll" href="index.php#work">WORK</a></li>
                       <li><a class="page-scroll" href="index.php#contact">CONTACT</a></li>
                       <li class="dropdown">
-                            <a href="#" class="dropdown-toggle hidden-md hidden-lg" data-toggle="dropdown">SOCIAL<span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle hidden-lg" data-toggle="dropdown">SOCIAL<span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
-                              <li><a href="#" class="facebook">Facebook</a></li>
-                              <li><a href="#" class="twitter">Twitter</a></li>
-                              <li><a href="#" class="linkedin">Linkedin</a></li>
-                              <li><a href="#" class="news">News</a></li>
+                              <li><a href="https://www.facebook.com/VisualLatina" class="facebook">Facebook</a></li>
+                              <li><a href="https://twitter.com/VisualLatina" class="twitter">Twitter</a></li>
+                              <li><a href="https://www.linkedin.com/company/visual-latina" class="linkedin">Linkedin</a></li>
+                              <li><a href="news.php" class="news">News</a></li>
                             </ul>
                         </li>
                       </ul>
                       <ul class="icon-r-social">
-                        <li class="new">News <a href="#" class="news-n"></a></li>
-                        <li><a href="#" class="facebook-f"></a></li>
-                        <li><a href="#" class="twitter-t"></a></li>
-                        <li><a href="#" class="linkedin-l"></a></li>
+                        <li class="new">News <a href="news.php" class="news-n"></a></li>
+                        <li><a href="https://www.facebook.com/VisualLatina" class="facebook-f"></a></li>
+                        <li><a href="https://twitter.com/VisualLatina" class="twitter-t"></a></li>
+                        <li><a href="https://www.linkedin.com/company/visual-latina" class="linkedin-l"></a></li>
                       </ul>
                   </div>
                 </div>
