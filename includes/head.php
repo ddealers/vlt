247a0c0a5d30cf0bf4d17ddbe4aed5d43fa70e54
Wenceslao Negrete <vampediem@gmail.com>
2014-08-21 23:30:12 -0500

    see more slider

--- includes/head.php
+++ includes/head.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="css/colorbox.css">
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="js/jquery.easing.min.js"></script>
+    <!--<script src="js/jquery.easing.min.js"></script>-->
     <script src="js/jquery.colorbox.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
