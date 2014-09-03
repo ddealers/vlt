11ae5945f9b4bba1b69daf5db8faaecfd715a666
aresavazquez <ares _vb>
2014-09-03 15:44:44 -0500

    correcciones vlt

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','Off');
 require_once('lib/PDO/config.php');
 require_once('lib/PDO/PDODB.php');
 require_once('lib/PDO/PDOResult.php');
@@ -39,7 +41,6 @@ $news = $n->all(3)->UTF8Encode()->get();
 									<h1>FIFA WORLD CUP<span>TM<br></span>BRAZIL 2014<span class="borde-cocacola"></span></h1>
 									<h2 class="tittle">Here we come!</h2>
 									<p class="tittle parrafo">Coca-Cola, official partner in this one-of-a-kind event, has commissioned us to develop its Shopper Marketing Platform for the US General Market.</p>
-									<p class="tittle">Coca-Cola, official partner in this one-of-a-kind event, has commissioned us to develop its Shopper Marketing Platform for the US General Market.</p>
 									<p><a class="btn btn-lg btn-primary" href="caso_coca.php" role="button">SEE MORE<img src="./img/play.png" class="play"></a></p>
 								</div>
 								<div class="col-xs-12 col-sm-8">
@@ -53,11 +54,10 @@ $news = $n->all(3)->UTF8Encode()->get();
 						<div class="container">
 							<div class="carousel-caption row">
 								<div class="col-xs-12 col-sm-4">
-									<p class="tittle">Philips Female Beauty Europe</p>
+									<p class="tittlehead">Philips Female Beauty Europe</p>
 									<h1>A SANCTUARY<br> FOR BEAUTY<span class="borde-cocacola"></h1>
 									<h2 class="tittle">A smoothing purchase</h2>
 									<p class="tittle parrafo">In a category where woman find the experience too daunting  and too masculine, we combined crucial female insights with a sophisticated approach to bring to life Philips Female Beauty products.</p>
-									<p class="tittle">In a category where woman find the experience too daunting  and too masculine, we combined crucial female insights with a sophisticated approach to bring to life Philips Female Beauty products.</p>
 									<p><a class="btn btn-lg btn-primary" href="caso_philips.php" role="button">SEE MORE<img src="./img/play.png" class="play"></a></p>
 								</div>
 								<div class="col-xs-12 col-sm-8">
@@ -71,15 +71,14 @@ $news = $n->all(3)->UTF8Encode()->get();
 						<div class="container">
 							<div class="carousel-caption row">
 								<div class="col-xs-12 col-sm-4">
-									<p class="tittle">Zucaritas Latin America Toolkit</p>
+									<p class="tittlehead">Zucaritas Latin America Toolkit</p>
 									<h1>BRING HOME<br> A ROARING BREAKFAST<span class="borde-cocacola"></h1>
 									<h2 class="tittle">Kick-start achievements</h2>
 									<p class="tittle parrafo">Stepping up to the challenge of switching shoppers, we boosted Zucaritas nutrition energy into accomplishments, becoming Moms' ally while helping their children perform at their best.</p>
-									<p class="tittle">Stepping up to the challenge of switching shoppers, we boosted Zucaritas nutrition energy into accomplishments, becoming Moms' ally while helping their children perform at their best.</p>
 									<p><a class="btn btn-lg btn-primary" href="caso_kellogs.php" role="button">SEE MORE<img src="./img/play.png" class="play"></a></p>                       
 								</div>
 								<div class="col-xs-12 col-sm-8">
-									<img src="./img/zucaritas.png" class="imagen">
+									<img src="./img/zucaritas.png" class="imagen zuca">
 								</div>
 							</div>
 						</div>
@@ -89,11 +88,10 @@ $news = $n->all(3)->UTF8Encode()->get();
 						<div class="container">
 							<div class="carousel-caption row">
 								<div class="col-xs-12 col-sm-4 slider-colgate">
-									<p class="tittle">Colgate México "Tres pasitos"</p>
+									<p class="tittlehead">Colgate México "Tres pasitos"</p>
 									<h1 >EDUCATION <br> THAT ENTERTRAINS<span class="borde-cocacola"></h1>
 									<h2 class="tittle">A tale of oral care</h2>
 									<p class="tittle parrafo">Stepping up to  the challenge of switching shoppers,we boosted Zucaritas nutrition, turning energy into accomplishments, becoming Moms' ally while helping their children perform at their best.</p>
-									<p class="tittle">Stepping up to  the challenge of switching shoppers,we boosted Zucaritas nutrition, turning energy into accomplishments, becoming Moms' ally while helping their children perform at their best.</p>
 									<p><a class="btn btn-lg btn-primary" href="caso_colgate.php" role="button">SEE MORE<img src="./img/play.png" class="play"></a></p>
 								</div>
 								<div class="col-xs-12 col-sm-8">  
@@ -116,7 +114,7 @@ $news = $n->all(3)->UTF8Encode()->get();
 			<div class="col-xs-12 col-sm-6">
 				<div class="bloque">
 					<h3 class="verde">In the age <br> of digital shoppers</h3>
-					<h2 class="featurette-heading"><span class="cabeza"></span>MARKETING IS LIMITLESS!<span class="pies"></span></h2>
+					<h2 class="featurette-heading"><span class="cabeza"></span>MARKETING <br> IS LIMITLESS!<span class="pies"></span></h2>
 					<p class="porcentaje"> <span>44%</span><br>use mobile to research products in the beginning of a shopping trip</p>
 					<p class="porcentaje2"> <span>21%</span><br>who started with a local search ended up marking the purchase online</p>
 					<p class="liga">Numbers for Argentina 2013 from http://think.withgoogle.com/mobileplanet/en/</p>
@@ -133,7 +131,7 @@ $news = $n->all(3)->UTF8Encode()->get();
 					<img src="./img/icon-WHY.png" class="icon1">
 					<h2 class="text-tittle">Who we are</h2>
 					<p class="contenido see">Our vast experience across multiple markets <br> empowers us to understand unique shopping<br> behaviors in many different cultures.</p>
-					<a href="about.php#why" class="icons">SEE MORE<img src="./img/play2.png" class="play"></a>
+					<a href="about.php" class="icons">SEE MORE<img src="./img/play2.png" class="play"></a>
 				</div><!-- /.col-lg-4 -->
 				<div class="col-sm-6 col-md-3">
 					<img src="./img/mundo_marcado.png" class="icon2">
@@ -177,20 +175,20 @@ $news = $n->all(3)->UTF8Encode()->get();
 			</div>
 		</div>
 <!-- ******************************************** -->
-		<div class="row featurette-divider car">
+		<div class="row featurette challenge tailor">
 			<div class="col-md-5 col-sm-6">
 				<img src="./img/sep3-elemento1.png" class="car">
 			</div>
 			<div class="col-md-7 col-sm-6">
 				<h2 class="tittle-all">All shoppers are diverse, <br> so is every shopping trip<span class="verde"></span></h2>
-				<h1 class="solution-every">EVERY CHALLENGE <br> REQUIRES<span class="text-green"><br>A TAILOR MADE <br> SOLUTION</span></h1>
+				<h1 class="colorBlanco">EVERY CHALLENGE <br> REQUIRES<span class="text-green"><br>A TAILOR MADE <br> SOLUTION</span></h1>
 			</div>
 		</div>
 <!-- ******************************************** -->
 		<div id="work" class="row featurette-clients y">
 			<div class="col-md-8 fotoProducto">
-				<h2 class="tittle empresa">We create<br> tailor-made solutions<span class="made-s"></span></h2>
-				<h1 class="solution-text">FOR A VARIETY OF CATEGORIES AND SHOPPER MINDSETS</h1>
+				<h2 class="tittle empresa">We create<br> tailor-made solutions<span class="made-s"></span><span class="verde"></span></h2>
+				<h1 class="solution-text variety">FOR A VARIETY OF CATEGORIES AND SHOPPER MINDSETS</h1>
 			</div>
 			<div class="moreinfo col-sm-12 col-md-4">
 				<img src="./img/coca-2.png" class="marca-co">
@@ -224,7 +222,7 @@ $news = $n->all(3)->UTF8Encode()->get();
 <!-- ******************************************** -->
 		<div class="row featurette-hand">
 			<div class="col-md-7 womenB">
-				<h2 class="tittle-women">We change <br> people's shopping <br> behaviors <span class="botella"></span></h2>
+				<h2 class="tittle-women">We change <br> people's shopping <br> behaviors <span class="botella"></span><span class="verde"></span></h2>
 				<h1 class="h1Tittle when">WHEN THEY <br> ARE IN THE MOOD <br> TO SHOP</h1>
 			</div>
 		</div>
@@ -273,13 +271,13 @@ $news = $n->all(3)->UTF8Encode()->get();
 		</div>
 <!-- ******************************************** -->
 		<div class="row featurette-grisc">
-			<div class="col-md-7 col-sm-6">
-				<h1 class="titulo">WE' LOVE <br> TO HEAR <span class="we-loved"></span></h1>
+			<div class="col-md-7 col-sm-6 love">
+				<h1 class="titulo we">WE' LOVE <br> TO HEAR <span class="we-loved"></span><span class="verde"></span></h1>
 				<h2 class="tittle-love">about your <br> business <br> challenges</h2>
 			</div>
 			<div class="col-md-5 col-sm-6">
 				<img src="./img/sep5-elemento2.png" class="canasto">
-				<p class="leyend">Contact us and we'll get back to you with some provocative though starters.</p>
+				<p class="leyend canasta">Contact us and we'll get back to you with some provocative though starters.</p>
 			</div>
 		</div>
 <!-- ******************************************** -->
@@ -294,48 +292,48 @@ $news = $n->all(3)->UTF8Encode()->get();
 					<p class="direccion">We work for people all around the world, but if you are close to one of our offices let usknow so we can meet.</p>
 					<a href="Tel: +54 11 4797 9669" class="links"> <p>Tel: +54 11 4797 9669</p></a>
 					<a href="Cel: +54 911 4526 3905" class="links"> <p>Cel: +54 911 4526 3905</p> </a>
-					<a href="mailto:diego.blanco@visuallatina.com" class="links"><p>diego.blanco@visuallatina.com</p></a>
+					<a href="mailto:diego.blanco@visuallatina.com" class="links correo"><p>diego.blanco@visuallatina.com</p></a>
 					
 					<p class="pais">Atlanta</p>
 					<p class="direccion">1117 Peachtree Street Suite 1222, Atlanta , GA 30309.</p>
 					<a href="Tel: +1 404 630 9079" class="links"> <p>Tel: +1 404 630 9079</p> </a>
 					<a href="Cel: +1 678 772 0118" class="links"> <p>Cel: +1 678 772 0118</p> </a>
-					<a href="mailto:guadalupe.cano@visuallatina.com" class="links"><p>guadalupe.cano@visuallatina.com</p></a>
+					<a href="mailto:guadalupe.cano@visuallatina.com" class="links correo"><p>guadalupe.cano@visuallatina.com</p></a>
 					
 					<p class="pais">London</p>
 					<p class="direccion">Willoughby House, 439 Richmond Road Richmond-Upon-Thames, Surrey, London, TW1 2AG.</p>
 					<a href="Tel: +44 7872 171 400" class="links"> <p>Tel: +44 7872 171 400</p> </a>
 					<a href="Cel: +44 7980 595 323" class="links"> <p>Cel: +44 7980 595 323</p> </a>
-					<a href="mailto:simone.dyson@visuallatina.com" class="links"><p>simone.dyson@visuallatina.com</p></a>
+					<a href="mailto:simone.dyson@visuallatina.com" class="links correo"><p>simone.dyson@visuallatina.com</p></a>
 					
 					<p class="pais">New York</p>
 					<a href="Cel: +1 917 399 8770" class="links"> <p>Cel: +1 917 399 8770</p> </a>
-					<a href="mailto:claudia.orci@visuallatina.com" class="links"><p>claudia.orci@visuallatina.com</p></a>
+					<a href="mailto:claudia.orci@visuallatina.com" class="links correo"><p>claudia.orci@visuallatina.com</p></a>
 				</div>
 				<div class="col-sm-6 col-md-4">
 					<p class="pais">México D.F.</p>
 					<p class="direccion">Hegel No. 618 Col. Bosques de Chapultepec, Delegación Miguel Hidalgo , C.P. 11580.</p>
 					<a href="Tel: +52 55 3003 0530" class="links"> <p>Tel: +52 55 3003 0530</p> </a>
 					<a href="Cel: +52 155 2979 4416" class="links"> <p>Cel: +52 155 2979 4416</p> </a>
-					<a href="mailto:natalya.tatarin@visuallatina.com" class="links"><p>natalya.tatarin@visuallatina.com</p></a>
+					<a href="mailto:natalya.tatarin@visuallatina.com" class="links correo"><p>natalya.tatarin@visuallatina.com</p></a>
 					
 					<p class="pais">São Paulo</p>
 					<p class="direccion">Rua República do Iraque, 1585 Campo Belo São Paulo SP – Brasil CEP: 04611-003.</p>
 					<a href="Tel: +55 11 5093 0297" class="links"> <p>Tel: +55 11 5093 0297</p> </a>
 					<a href="Cel: +55 11 99722 4582" class="links"> <p>Cel: +55 11 99722 4582</p> </a>
-					<a href="mailto:renato.tura@visuallatina.com.br" class="links"><p>renato.tura@visuallatina.com.br</p></a>
+					<a href="mailto:renato.tura@visuallatina.com.br" class="links correo"><p>renato.tura@visuallatina.com.br</p></a>
 					
 					<p class="pais">Buenos Aires</p>
 					<p class="direccion">Gaspar Campos 647 Vicente Lopez , B1638ARK.</p>
 					<a href="Tel: +5411 4797 9669" class="links"> <p>Tel: +5411 4797 9669</p> </a>
 					<a href="Cel: +54 11 3596 6020" class="links"> <p>Cel: +54 11 3596 6020</p> </a>
-					<a href="mailto:eugenio.bory@visuallatina.com" class="links"><p>eugenio.bory@visuallatina.com</p></a>
+					<a href="mailto:eugenio.bory@visuallatina.com" class="links correo"><p>eugenio.bory@visuallatina.com</p></a>
 					
 					<p class="pais">Santiago de Chile</p>
 					<p class="direccion">Cerro Colorado 5240 Torre II, p10, of1015 Las Condes, Santiago , CP 7550000.</p>
 					<a href="Tel: +56 2 2434 5314" class="links"> <p>Tel: +56 2 2434 5314</p> </a>
 					<a href="Cel: +56 9 9276 2749" class="links"> <p>Cel: +56 9 9276 2749</p> </a>
-					<a href="mailto:gustavo.maspero@visuallatina.com" class="links"><p>gustavo.maspero@visuallatina.com</p></a>
+					<a href="mailto:gustavo.maspero@visuallatina.com" class="links correo"><p>gustavo.maspero@visuallatina.com</p></a>
 				</div>
 			</div>
 		</div>
